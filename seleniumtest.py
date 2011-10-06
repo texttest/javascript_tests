7b7ac96cbb50b0e14535e98284b774aef4c2eb6e
Geoff Bache <geoff.bache@jeppesen.com>
2011-10-06 13:49:55 +0200

    Refactorings, add sleep so it gets the results after the page loads

--- seleniumtest.py
+++ seleniumtest.py
@@ -5,29 +5,30 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 import time, sys, os
 
+def writeTableLines(filename):
+   with open(filename, 'w') as f1:
+      elements0 = driver.find_elements_by_tag_name("TR")
+      for el in elements0:
+         f1.write( el.text + '\n')
+   
+
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
 
 filename = os.path.abspath("overview.html")
 
 driver.get("file://" + filename)
-with open('f1.txt', 'w') as f1:
-   elements0 = driver.find_elements_by_tag_name("TR")
-   for el in elements0:
-      f1.write( el.text + '\n')
+writeTableLines('f1.txt')
 
 for line in sys.stdin:
     if len(line.strip()) > 0:
         element1 = driver.find_element_by_id(line.strip())
         element1.click()
 
-with open('f2.txt', 'w') as f2:
-   elements = driver.find_elements_by_tag_name("TR")
-   for e in elements:
-      f2.write( e.text + '\n')
+time.sleep(3)
+writeTableLines('f2.txt')
 
 try:
-    # we have to wait for the page to refresh, the last thing that seems to be updated is the title
     #WebDriverWait(driver, 10).until(lambda driver : driver.title.lower().startswith("cheese!"))
 
     print driver.title
