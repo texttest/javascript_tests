ebd1e82644c79b7ab7dfa48cce2a75b4802a7248
Geoff Bache <geoff.bache@jeppesen.com>
2011-10-07 12:19:25 +0200

    Updating and simplifying tests after changes

--- seleniumtest.py
+++ seleniumtest.py
@@ -5,33 +5,32 @@ from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 import time, sys, os
 
-def writeTableLines(filename):
-   with open(filename, 'w') as f1:
-      elements0 = driver.find_elements_by_tag_name("TR")
-      for el in elements0:
-         f1.write( el.text + '\n')
+def writeTableLines():
+   elements0 = driver.find_elements_by_tag_name("TR")
+   for el in elements0:
+      if el.is_displayed():
+         print el.text
    
 
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
 
-filename = os.path.abspath("overview.html")
-
-driver.get("file://" + filename)
-writeTableLines('f1.txt')
-
-for line in sys.stdin:
-    if len(line.strip()) > 0:
-        element1 = driver.find_element_by_id(line.strip())
-        element1.click()
-
-time.sleep(3)
-writeTableLines('f2.txt')
-
 try:
+    filename = os.path.abspath("overview.html")
+
+    driver.get("file://" + filename)
+    
+    for line in sys.stdin:
+        if len(line.strip()) > 0:
+            element1 = driver.find_element_by_id(line.strip())
+            element1.click()
+
+    time.sleep(1)
+    writeTableLines()
+    
     #WebDriverWait(driver, 10).until(lambda driver : driver.title.lower().startswith("cheese!"))
 
-    print driver.title
+    #print driver.title
 
 finally:
     driver.quit()
