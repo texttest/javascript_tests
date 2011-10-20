d551a2de9ab2bd5a08e0c645e1ac25fb96e3d86e
Geoff Bache <geoff.bache@jeppesen.com>
2011-10-20 14:24:08 +0200

    Adding test for regexps and attempt for ctrl-clicking. Second fails due to Selenium bug

--- seleniumtest.py
+++ seleniumtest.py
@@ -2,6 +2,7 @@
 
 from selenium import webdriver
 from selenium.common.exceptions import TimeoutException
+from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.support.ui import WebDriverWait # available since 2.4.0
 import time, sys, os
 
@@ -10,25 +11,35 @@ def writeTableLines():
    for el in elements0:
       if el.is_displayed():
          print el.text
-   
 
 # Create a new instance of the Firefox driver
 driver = webdriver.Firefox()
+
+def ctrl_click(element):
+   chain = webdriver.ActionChains(driver)
+   chain.key_down(Keys.CONTROL)
+   chain.click(element)
+   chain.key_up(Keys.CONTROL)
+   chain.perform()
 
 try:
     filename = os.path.abspath("overview.html")
 
     driver.get("file://" + filename)
-    
+    time.sleep(4)
     for unstrippedLine in sys.stdin:
         line = unstrippedLine.strip()
         if len(line) > 0:
             elementId = line.split(':')[0]
             action = line.split(':')[1]
             element = driver.find_element_by_id(elementId)
-            eval("element." + action)
-
-    time.sleep(1)
+            try:
+               eval("element." + action)
+            except AttributeError:
+               action_func = action.split("(")[0]
+               eval(action_func)(element)
+            
+        time.sleep(4)
     writeTableLines()
     
     #WebDriverWait(driver, 10).until(lambda driver : driver.title.lower().startswith("cheese!"))
